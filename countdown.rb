9bd4a1693d7e5dfcf6442ed180ac5d2b0e5927ac
sbalayan1 <balayans2014@yahoo.com>
2021-06-21 11:49:06 -0700

    commit tree 0216dc8d35ae1d83551e6fb4a763fdbd244168f5

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,14 @@
 #write your code here
 
-def countdown
+def countdown integer
+    while integer>=1
+        puts "#{integer} SECOND(S)!"
+        integer -= 1
+    end 
+
+    'HAPPY NEW YEAR!'
 end
+
+def countdown_with_sleep integer
+    sleep integer
+end 
